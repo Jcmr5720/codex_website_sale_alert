4b23eee338426a41e12833a771cbc53e175705b9
Jcmr5720 <camilomr1414@hotmail.com>
2025-10-22 16:57:41 -0500

    c3

--- sale_order_alert/__manifest__.py
+++ sale_order_alert/__manifest__.py
@@ -1,11 +1,16 @@
 {
-    'name': 'Sale Order Inform Button',
-    'version': '16.0.1.0.0',
-    'category': 'Sales',
-    'summary': 'Send WhatsApp notification from invoiced sale orders.',
-    'license': 'OPL-1',
-    'depends': ['sale_management', 'whatsapp_connector_sale'],
-    'data': ['views/sale_order_views.xml'],
+    'name': 'Sale_Order_Alert',
+    'version': '1.0.0',
+    'category': 'Tools',
+    'author': 'Juan Camilo Muñoz',
+    'summary': 'Extra notificaciones a cliente',
+    'depends': [
+        'sale_management',
+        'whatsapp_connector_sale'],
+    'data': [
+        'views/sale_order_views.xml'],
     'installable': True,
-    'application': False,
+    'application': True,
+    'auto_install': False,
+    'license': 'GPL-3'
 }
