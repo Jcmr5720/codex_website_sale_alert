ad82a5e1a6c83e3e43bf25e313dee966d7fb59e6
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-10-24 18:58:36 -0500

    Fix manifest formatting for sale order alert

--- sale_order_alert/__manifest__.py
+++ sale_order_alert/__manifest__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 {
     'name': 'Sale_Order_Alert',
     'version': '1.0.0',
@@ -7,13 +9,15 @@
     'depends': [
         'sale_management',
         'sale_stock',
-        'whatsapp_connector_sale'],
+        'whatsapp_connector_sale',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/sale_order_alert_wizard_views.xml',
-        'views/sale_order_views.xml'],
+        'views/sale_order_views.xml',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
-    'license': 'GPL-3'
+    'license': 'GPL-3',
 }
